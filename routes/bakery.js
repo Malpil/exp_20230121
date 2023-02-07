b91248c57fcc0a794dcd34c16b3ebe32f0d221be
Malpil <120145655+Malpil@users.noreply.github.com>
2023-02-07 17:49:27 +0900

    230207백엔드프로젝트
    230207백엔드 프로젝트

--- routes/bakery.js
+++ routes/bakery.js
@@ -55,43 +55,6 @@ router.post('/insertshop.json', upload.single("file"), async function (req, res,
     }
 });
 
-
-// 지역별 상점 목록 => 127.0.0.1:3000/api/bakery/storelist.json?page=1&text=검색어
-// router.get('/storelist.json', async function (req,res,next) {
-//   try{
-//     const text = req.query.text; //검색어
-//     const page = req.query.page; //1
-    
-//     //전체 데이터에서 제목이 검색어가 포함된 것 가져오기
-//     //a => a123 => a라는 데이터를 찾으면 a123이라는 데이터중 a가 포함되어 있어서 불러 올 수 있게함
-//     const query = {address: new RegExp(text, 'i') }; //RegExp(포함된 것을 찾아내는 함수)
-//     const project = {
-//       filedata: 0,
-//       filename: 0,
-//       filesize: 0,
-//       filetype: 0,
-//     };
-//     const result = await Bakery.find(query,project)
-//       .sort({_id: -1})
-//       .skip({page: - 1} * 10)
-//       .limit(10);
-      
-//       //등록일, 이미지URL 수동 생성
-//       for (let tmp of result) {
-//         //format("YYYY-MM-DD HH:mm:ss")
-//         tmp.regdate1 = moment(tmp.regdate).format("YYYY-MM-DD");
-//         tmp.imageurl = `/api/bakery/image?_id=${tmp._id}&ts=${Date.now()};`
-//       }
-
-//     //페이지네이션용 전체 개수
-//     const total = await Bakery.countDocuments(query);
-      
-//     return res.send({ state: 200, total: total, result: result });
-//   } catch(e) {
-//     console.error(e);
-//     return res.send({ status: -1, result: e});
-//   }
-// });
 //지역별 상점 목록 => 127.0.0.1:3000/api/bakery/selectshop.json?page=1&text=검색어
 router.get('/selectshop.json', async function (req, res, next) {
   try {
@@ -123,9 +86,33 @@ router.get('/selectshop.json', async function (req, res, next) {
     const total = await Bakery.countDocuments(query);
 
     return res.send({ status: 200, total: total, result: result });
+  } catch (e) { 
+    console.error(e);
+    return res.send({ status: -1, result: e });
+  }
+
+});
+
+//이미지 URL => 127.0.0.1:3000/api/bakery/image?_id=3
+//<img src="/api/bakery/image?_id=3">
+router.get('/image', async function (req, res, next) {
+  try {
+
+    const query = { _id: Number(req.query._id) };
+    const project = { filedata: 1, filetype: 1 };
+    const result = await Bakery.findOne(query, project);
+    //console.log(result);
+
+    res.contentType(result.filetype);
+
+    return res.send(result.filedata);
+
   } catch (e) {
     console.error(e);
     return res.send({ status: -1, result: e });
   }
-});
+}); 
+
+
+
 module.exports = router;
